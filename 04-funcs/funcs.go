289fe5b41a6e53b8985076e6e30a638500b13a53
Wolfram Kleis <wkleis@users.noreply.github.com>
2020-06-14 15:34:37 +0200

    closure demo: inc

--- 04-funcs/funcs.go
+++ 04-funcs/funcs.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+type context struct {
+	size   int
+	number int
+	name   string
+}
+
 func main() {
 	//function expression
 	funcs := []func(string){
@@ -25,15 +31,32 @@ func main() {
 	//function as return value
 	f := foo("Hurz")
 	fmt.Println(f())
+	ctx := context{
+		size:   2,
+		number: 11,
+		name:   "foo",
+	}
+	f2 := foo2(ctx)
+	fmt.Println(f2())
 	bar("Hurz",
 		func(s string) {
 			fmt.Println("callback called with: ", s)
 		})
+	closureDemo()
+
 }
 
+//function that returns function with string variable bound to varibale from function closure
 func foo(s string) func() string {
 	return func() string {
 		return fmt.Sprintf("The word is: %v", s)
+	}
+}
+
+//function that returns functions with struct variable bound to variable from function closure
+func foo2(c context) func() string {
+	return func() string {
+		return fmt.Sprintf("size: %d, number: %d, %s", c.size, c.number, c.name)
 	}
 }
 
@@ -41,4 +64,24 @@ func foo(s string) func() string {
 func bar(s string, cb func(s string)) {
 	x := fmt.Sprintf("The message is '%s'!!!", s)
 	cb(x)
+}
+func closureDemo() {
+	i1 := incrementor()
+	i2 := incrementor()
+	fmt.Println(i1())
+	fmt.Println(i1())
+	fmt.Println(i1())
+	fmt.Println(i1())
+	fmt.Println(i2())
+	fmt.Println(i2())
+	fmt.Println(i2())
+
+}
+
+func incrementor() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
 }
