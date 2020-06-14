1d743ef67a4cbfc06f3ee9cf34504c810d98a45f
Wolfram Kleis <wkleis@users.noreply.github.com>
2020-06-14 16:22:37 +0200

    interface exercise

---
+++ 05-interface/interface.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	a float64
+}
+type circle struct {
+	r float64
+}
+
+func (s square) area() float64 {
+	return s.a * s.a
+}
+func (c circle) area() float64 {
+	return c.r * c.r * math.Pi
+}
+
+type shape interface {
+	area() float64
+}
+
+func main() {
+	squares := []square{{5.5}, {2.0}, {3.99}}
+	circles := []circle{{1.0}, {2.0}, {3}, {77.24}}
+	n := len(squares) + len(circles)
+	pos := 0
+	shapes := make([]shape, n, n)
+	fmt.Println("squares")
+	for _, x := range squares {
+		fmt.Printf("%v\t%v\n", x, x.area())
+		shapes[pos] = x
+		pos++
+	}
+	fmt.Println("Circles")
+	for _, x := range circles {
+		fmt.Printf("%v\t%v\n", x, x.area())
+		shapes[pos] = x
+		pos++
+	}
+
+	fmt.Println("Shapes")
+	for _, x := range shapes {
+		fmt.Printf("%T\t%v\t%v\n", x, x, x.area())
+	}
+}
