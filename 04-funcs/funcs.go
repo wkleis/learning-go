31c3d77b9e64d6a3e1c053a49fcad943bd90dd6a
Wolfram Kleis <wkleis@users.noreply.github.com>
2020-06-14 15:11:31 +0200

    functions as return values

--- 04-funcs/funcs.go
+++ 04-funcs/funcs.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	//function expression
 	funcs := []func(string){
 		func(s string) {
 			fmt.Println("foo:", s)
@@ -19,5 +20,15 @@ func main() {
 	for _, f := range funcs {
 		fmt.Printf("%T\n", f)
 		f("Hurz")
+	}
+	fmt.Println("-------------")
+	//function as return value
+	f := foo("Hurz")
+	fmt.Println(f())
+}
+
+func foo(s string) func() string {
+	return func() string {
+		return fmt.Sprintf("The word is: %v", s)
 	}
 }
