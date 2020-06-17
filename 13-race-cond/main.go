86d8ea389f547a5ec126529ff1a3e4787653924e
Wolfram Kleis <wkleis@users.noreply.github.com>
2020-06-17 12:55:28 +0200

    add simple race condition example

---
+++ 13-race-cond/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("NumCPU", runtime.NumCPU())
+	fmt.Println("NumGoRoutine", runtime.NumGoroutine())
+	counter := 0
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+			fmt.Println("NumGoRoutine", runtime.NumGoroutine())
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Counter", counter)
+}
