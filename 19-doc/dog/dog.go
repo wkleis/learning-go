b5072e07153338a853c7ccc452ff8cffb1ae7b0b
Wolfram Kleis <wkleis@users.noreply.github.com>
2020-06-19 15:15:40 +0200

    update dog package ;-)

--- 19-doc/dog/dog.go
+++ 19-doc/dog/dog.go
@@ -1,6 +1,5 @@
-// execrcise from Udemy course https://www.udemy.com/course/learn-how-to-code/ by Todd McLeod
-
 //Package dog contains useless functions about the world of dogs
+//This is an exercise from Udemy course https://www.udemy.com/course/learn-how-to-code/ by Todd McLeod
 package dog
 
 //Years returns the number of dog years that correspond to to a given number of human years
